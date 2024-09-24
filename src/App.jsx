01c8c84a3b6ef08015ec45124cd5cd77c3addacf
Victor Wade <p2525657888@gmail.com>
2024-09-24 05:40:05 -1000

    add data, useState, map the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { puppy_list } from "./data.js";
 import "./App.css";
 
 function App() {
-	return <div></div>;
+	const [puppies, set_puppies] = useState(puppy_list);
+
+	console.log(puppies);
+	return (
+		<div>
+			{puppies.map((puppy) => {
+				return <p>{puppy.name}</p>;
+			})}
+		</div>
+	);
 }
 
 export default App;
